390e790a691c4f21f17e24c1ac7e8050d4925955
abdel2000-dply <abno870@gmail.com>
2023-12-22 20:25:43 +0100

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,14 +1,20 @@
-# install flask version 2.1.0
-
+# install python3.8
 package { 'python3.8':
   ensure => '3.8.10',
 }
 
+# Install pip
+package { 'python3-pip':
+  ensure => present,
+}
+
+# install flask version 2.1.0
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
 
+# Install Werkzeug
 package { 'Werkzeug':
   ensure => '2.1.1',
   provider => 'pip',
