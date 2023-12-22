7c867b5e2f92f870bba45c64d1f1777b46159ce3
abdel2000-dply <abno870@gmail.com>
2023-12-22 20:00:31 +0100

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,5 +4,5 @@ package { 'install_flask':
   command => '/usr/bin/pip3 install Flask==2.1.0',
   path    => '/usr/local/bin:/usr/bin',
   unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
-  require => Package['python3-pip'],
+  require => Package['python3-pip']
 }
