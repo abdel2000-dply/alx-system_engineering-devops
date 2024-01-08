e54c1199a038a531c900569c2ed4cd7c033ebb1d
abdel2000-dply <abno870@gmail.com>
2024-01-08 22:01:58 +0100

    task 2;

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,5 @@
+# Add a custom HTTP header (on web-01 and web-02)
+exec { 'http header':
+	  command  => 'sudo apt-get update -y && sudo apt-get install nginx -y && sudo sed -i "s/server_name _;/server_name _;\n\tadd_header X-Served-By \"$hostname\";/" /etc/nginx/sites-available/default && sudo service nginx restart',
+			     provider => shell,
+}
