9aa012b458513645304171f70b494eb378f5ba64
abdel2000-dply <abno870@gmail.com>
2024-01-29 22:29:09 +0100

    task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Gather data from an API """
+import requests
+from sys import argv
+
+
+def todo_progress(user_id):
+    info_url = f'https://jsonplaceholder.typicode.com/users/{user_id}'
+    todos_url = f'https://jsonplaceholder.typicode.com/users/{user_id}/todos'
+    todos_response = requests.get(todos_url)
+    info_response = requests.get(info_url)
+
+    todos = todos_response.json()
+    info = info_response.json()
+
+    total_tasks = len(todos)
+    completed_tasks = sum(todo['completed'] for todo in todos)
+
+    print("Employee {} is done with tasks({}/{}):".format(
+        info['name'], completed_tasks, total_tasks
+    ))
+
+    for todo in todos:
+        if todo['completed']:
+            print(f"    {todo['title']}")
+
+
+if __name__ == "__main__":
+    user_id = int(argv[1])
+    todo_progress(user_id)
