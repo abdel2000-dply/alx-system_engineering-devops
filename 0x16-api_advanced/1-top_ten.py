aa7254f07e0e345dc63f697b1ec20018d58b9876
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-02-13 15:35:00 +0100

    task 1: top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""
+This module contains a function that queries the Reddit API
+"""
+import requests
+
+headers = {'User-Agent': 'Mozilla/5.0'}
+
+
+def top_ten(subreddit):
+    """Prints the titles of the first 10 hot posts"""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    response = requests.get(url, allow_redirects=False, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        posts = data['data']['children']
+        for post in posts:
+            title = post['data']['title']
+            print(title)
+    else:
+        print(None)
