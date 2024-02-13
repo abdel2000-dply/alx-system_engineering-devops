c23f4ed56043cce33717d5301c9fd5de77087a09
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-02-13 14:28:20 +0100

    task 0: How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+""" 
+This module contains a function that queries the Reddit API
+and returns the number of subscribers for a given subreddit.
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ Returns the number of subscribers for a given subreddit. """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
+        return 0
