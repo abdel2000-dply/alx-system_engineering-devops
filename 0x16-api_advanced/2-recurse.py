a4936b5e81ef0c0d5bcf226a0c1d64edeab197f4
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-02-13 17:34:18 +0100

    task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""  Recurse it! """
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """ Recurse it! """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {"User-Agent": "MyCustomUserAgent/1.0"}
+    params = {"limit": 100, "after": after}
+    response = requests.get(
+        url, allow_redirects=False, headers=headers, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        children = data.get("data", {}).get("children", [])
+        for child in children:
+            title = child.get("data", {}).get("title")
+            hot_list.append(title)
+
+        after = data.get("data", {}).get("after")
+        if after is not None:
+            return recurse(subreddit, hot_list, after)
+        else:
+            return hot_list
+
+    return None
