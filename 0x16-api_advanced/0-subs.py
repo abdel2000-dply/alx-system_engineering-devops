da10aeffaf498d695ac4e4955b5a00828b5102a3
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-02-13 18:10:47 +0100

    fixing issue

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ import requests
 def number_of_subscribers(subreddit):
     """ Returns the number of subscribers for a given subreddit. """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers={'User-Agent': '0x16-api_advanced:project:\
+    headers = {'User-Agent': '0x16-api_advanced:project:\
              v1.0.0 (by /u/dayouness)'}
     response = requests.get(url, allow_redirects=False, headers=headers)
 
