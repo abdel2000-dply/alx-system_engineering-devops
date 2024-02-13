2b6dfccc6486f7173f2d204f7a4a82ea8ca0a9f5
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-02-13 18:14:31 +0100

    Update User-Agent header in API request

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,8 +9,7 @@ import requests
 def number_of_subscribers(subreddit):
     """ Returns the number of subscribers for a given subreddit. """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': '0x16-api_advanced:project:\
-             v1.0.0 (by /u/dayouness)'}
+    headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, allow_redirects=False, headers=headers)
 
     if response.status_code == 200:
